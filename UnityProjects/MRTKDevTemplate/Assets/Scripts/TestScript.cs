f5992035dbf6ea703bbad38bf1555b4f953c56ec
Risto Rangel Kuoppa <risto.rangelkuoppa@microsoft.com>
2024-03-19 13:11:22 -0700

    Reproduction attempt

---
+++ UnityProjects/MRTKDevTemplate/Assets/Scripts/TestScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using MixedReality.Toolkit.UX;
+using UnityEngine.UI;
+
+
+namespace TMPro.Examples
+{
+    
+    public class TestScript : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject testButton;
+
+        void Start()
+        {
+            var testButtonVisualizer = testButton.GetComponent<StateVisualizer>();
+            var childrenRawImages = testButtonVisualizer.GetComponentsInChildren<RawImage>();
+            var backPlateRawImage = childrenRawImages[0];
+            backPlateRawImage.color = new Color32(0, 255, 0, 255);
+        }
+
+
+        void Update()
+        {
+        }
+
+    }
+}
